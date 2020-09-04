77bd5313979997f3dad4999bb7c128ef149147a0
Miyazaki Masatoshi <ma.miyazaki@soraniwa.co.jp>
2020-09-04 15:21:17 +0900

    feat: add sample code

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ma-miyazaki/go-neo4j-template/internal"
+)
+
+func main() {
+	fmt.Printf("Hello world.\n")
+
+	defer internal.Neo4j.Close()
+
+	_, err := internal.Neo4j.DeleteAll()
+	if err != nil {
+		fmt.Println("err", err)
+		return
+	}
+}
